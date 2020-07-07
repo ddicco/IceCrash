5275bc79c07f1a74fdc0fb47b446a9efc3bb6494
ddicco <45797759+ddicco@users.noreply.github.com>
2020-07-07 13:44:49 -0300

    Update IceCrashListener.java

--- me/ddicco/icecrash/IceCrashListener.java
+++ me/ddicco/icecrash/IceCrashListener.java
@@ -1,28 +1,45 @@
-package me.ddicco.IceCrash;
+package me.ddicco.icecrash;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAnimationEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ability.CoreAbility;
 
-public class IceCrashListener implements Listener
-{
-        @EventHandler(priority = EventPriority.NORMAL)
-        public void Sneak(PlayerToggleSneakEvent event)
-        {
-        	Player player = event.getPlayer();
-        	BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-        	
-        	if(event.isCancelled() || bPlayer == null) {
-        		return;
-        	} else if (bPlayer.canBend(CoreAbility.getAbility(IceCrash.class)) && !CoreAbility.hasAbility(event.getPlayer(), IceCrash.class)) {
-    			new IceCrash (player);
-        	}
-        	
-        }
- 
+
+public class IceCrashListener implements Listener {
+	
+	@EventHandler
+	public void onClick(PlayerToggleSneakEvent e) {
+		Player player = e.getPlayer();
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		
+		if (e.isCancelled() || bPlayer == null) {
+			return;
+		} else if (bPlayer.canBend(CoreAbility.getAbility(IceCrashPrepare.class)) && !CoreAbility.hasAbility(player, IceCrashPrepare.class) && !CoreAbility.hasAbility(player, IceCrash.class)) {
+			new IceCrashPrepare(player);
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerAnimationEvent(PlayerAnimationEvent event) {
+		
+		Player player = event.getPlayer();
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		
+		if (event.isCancelled() || bPlayer == null) {
+			return;
+		} else if (bPlayer.canBend(CoreAbility.getAbility(IceCrashPrepare.class))) {
+			if(CoreAbility.hasAbility(event.getPlayer(), IceCrashPrepare.class)) {
+				if(CoreAbility.hasAbility(event.getPlayer(), IceCrash.class)) {
+					((IceCrash) CoreAbility.getAbility(player, IceCrash.class)).leftClickFunction();
+				} else {
+					new IceCrash(player);
+				}
+			}
+		}
+	}
 }
